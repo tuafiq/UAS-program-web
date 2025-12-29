d49b55b7bb85f40e86aee013a283a1cbd4cf8dbe
taufiq <tuafiq8214829@gmail.com>
2025-12-30 02:21:38 +0700

    commit ke 2

---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-boostrap";
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-light text-center py-3 mt-4 w-100">
+      <Container>
+        <small> 2025 Berita Online || Dibuat dengan React $ boostrap</small>
+      </Container>
+    </footer>
+  );
+}
